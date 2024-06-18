4565060d1a2db9efed61f03dbc0ccaa883b288c9
savannstm <savannstm@gmail.com>
2024-06-18 19:21:33 +0700

    Remove unused `unzip` function, remove unuzed `zip-extract` dependency

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -1,11 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use regex::escape;
-use std::io::Cursor;
-use std::path::Path;
-use std::{fs::remove_file, path::PathBuf};
-use tauri::{generate_context, App, Builder, Event, Manager};
-use zip_extract::extract;
+use std::path::PathBuf;
+use tauri::{api::os::locale, generate_context, App, Builder, Event, Manager};
 mod writer;
 
 #[tauri::command]
@@ -13,16 +10,9 @@ fn escape_text(text: String) -> String {
     escape(&text)
 }
 
-#[tauri::command]
-fn unzip(path: &str, dest: &str) {
-    let bytes: Vec<u8> = std::fs::read(path).unwrap();
-    extract(Cursor::new(bytes), Path::new(dest), false).unwrap();
-    remove_file(path).unwrap();
-}
-
 fn main() {
     Builder::default()
-        .invoke_handler(tauri::generate_handler![escape_text, unzip])
+        .invoke_handler(tauri::generate_handler![escape_text])
         .setup(|app: &mut App| {
             let main_window: tauri::Window = app.get_window("main").unwrap();
             #[cfg(debug_assertions)]
@@ -35,7 +25,7 @@ fn main() {
                 .listen("compile", move |event: Event| {
                     writer::main(
                         PathBuf::from(event.payload().unwrap().replace('"', "")),
-                        tauri::api::os::locale().unwrap().as_str(),
+                        locale().unwrap().as_str(),
                     );
 
                     main_window.emit("compile-finished", "").unwrap();
