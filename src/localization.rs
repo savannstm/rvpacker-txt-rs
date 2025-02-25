6ef633abb7c46de4462eaa1e2717fdde79be70ce
savannstm <savannstm@gmail.com>
2025-02-25 17:35:06 +0500

    Update localization

--- src/localization.rs
+++ src/localization.rs
@@ -19,11 +19,8 @@ pub struct Localization<'a> {
     pub json_command_desc: &'a str,
 
     // Argument descriptions
-    pub input_dir_arg_read_desc: &'a str,
-    pub input_dir_arg_write_desc: &'a str,
-
-    pub output_dir_arg_read_desc: &'a str,
-    pub output_dir_arg_write_desc: &'a str,
+    pub input_dir_arg_desc: &'a str,
+    pub output_dir_arg_desc: &'a str,
 
     pub disable_processing_arg_desc: &'a str,
 
@@ -47,6 +44,9 @@ pub struct Localization<'a> {
     pub generate_json_command_desc: &'a str,
     pub write_json_command_desc: &'a str,
 
+    pub version_flag_desc: &'a str,
+    pub ignore_flag_desc: &'a str,
+
     // Argument types
     pub mode_arg_type: &'a str,
     pub input_path_arg_type: &'a str,
@@ -67,13 +67,14 @@ pub struct Localization<'a> {
     pub could_not_determine_game_engine_msg: &'a str,
     pub game_ini_file_missing_msg: &'a str,
     pub enabling_maps_processing_mode_metadata_msg: &'a str,
+    pub purge_args_incompatible_with_preserve_mode_msg: &'a str,
+    pub ignore_file_does_not_exist_msg: &'a str,
+    pub could_not_decrypt_ini_file_msg: &'a str,
 
     // Misc
     pub possible_values: &'a str,
     pub example: &'a str,
     pub default_value: &'a str,
-    pub when_reading: &'a str,
-    pub when_writing: &'a str,
     pub aliases: &'a str,
 }
 
@@ -89,7 +90,7 @@ impl Localization<'_> {
         Localization {
             // About message and templates
             about_msg: cstr!(
-                "<bold>This tool allows to parse RPG Maker XP/VX/VXAce/MV/MZ games text to .txt files and write them back to their initial form.</>"
+                r#"<bold>This tool allows to parse RPG Maker XP/VX/VXAce/MV/MZ games text to .txt files and write them back to their initial form. The program will always use "original" or "data" directories for original files, and "translation" directory to operate with translation files. It will also decrypt any .rgss encrypted archive if it's present.</>"#
             ),
             help_template: cstr!(
                 "{about}\n\n<underline,bold>Usage:</> rvpacker-txt-rs COMMAND [OPTIONS]\n\n<underline,bold>Commands:</>\n{subcommands}\n\n<underline,bold>Options:</>\n{options}"
@@ -100,43 +101,42 @@ impl Localization<'_> {
 
             // Command descriptions
             read_command_desc: cstr!(
-                r#"<bold>Parses files from "original" or "data" ("Data") folders of input directory to "translation" folder of output directory. If "Data" directory does not exist and there's an .rgss archive in the input directory, program automatically decrypts it.</>"#
+                r#"<bold>Parses game files, and decrypts .rgss archive if it's present.</>"#
             ),
             write_command_desc: cstr!(
-                r#"<bold>Writes translated files using original files from "original" or "data" ("Data") folders of input directory and writes results to "output" folder of output directory.</>"#
+                r#"<bold>Writes translated game files to the "output" directory.</>"#
             ),
-            purge_command_desc: cstr!(r#"<bold>Purges the unused lines from ".txt" translation files.</>"#),
+            purge_command_desc: cstr!(r#"<bold>Purges lines from ".txt" translation files.</>"#),
 
             json_help_template: cstr!("{about}\n\n<underline,bold>Commands:</>\n{subcommands}\n\n<underline,bold>Options:</>\n{options}"),
             json_command_desc: cstr!(r#"<bold>Provides the commands for JSON generation and writing.</>"#),
 
-            // Argument descriptions
-            input_dir_arg_read_desc: r#"Input directory, containing folder "original" or "data" ("Data") with original game files."#,
-            input_dir_arg_write_desc: r#"Input directory, containing folder "original" or "data" ("Data") with original game files, and folder "translation" with translation .txt files."#,
-
-            output_dir_arg_read_desc: r#"Output directory, where a "translation" folder with translation .txt files will be created."#,
-            output_dir_arg_write_desc: r#"Output directory, where an "output" folder with "data" ("Data") and/or "js" subfolders with game files with translated text from .txt files will be created."#,
+            input_dir_arg_desc: r#"Input directory, containing game files."#,
+            output_dir_arg_desc: r#"Output directory to output files to."#,
 
             disable_processing_arg_desc: "Skips processing specified files. plugins can be used interchangeably with scripts.",
 
-            romanize_desc: r#"If you parsing text from a Japanese game, that contains symbols like 「」, which are just the Japanese quotation marks, it automatically replaces these symbols by their roman equivalents (in this case, ''). This flag will automatically be used when writing if you parsed game text with it."#,
+            romanize_desc: "If you parsing text from a Japanese game, that contains symbols like 「」, which are just the Japanese quotation marks, it automatically replaces these symbols by their roman equivalents (in this case, '').\nThis argument will automatically be set on write/read with --mode append/purge commands if you parsed game text with it.",
 
-            disable_custom_processing_desc: "Disables built-in custom processing, implemented for some games. This flag will automatically be used when writing if you parsed game text with it.",
+            disable_custom_processing_desc: "Disables built-in custom processing, implemented for some games.\nThis argument will automatically be set on write/read with --mode append/purge commands if you parsed game text with it.",
             language_arg_desc: "Sets the localization of the tool to the selected language.",
 
             log_arg_desc: "Enables logging.",
             help_arg_desc: "Prints the program's help message or for the entered subcommand.",
 
             processing_mode_arg_desc: "How to process files. default - Aborts processing if encounters already existing translation .txt files.\nappend - For example, if game you're translating updates, you can use this flag to append any new text to your existing files preserving lines order.\nforce - Force rewrites existing translation .txt files.",
-            maps_processing_mode_arg_desc: cstr!("How to process maps.\ndefault - Ignore all previously encountered text duplicates\nseparate - For each new map, reset the set of previously encountered text duplicates <bold>RECOMMENDED!</>\npreserve - Allow all text duplicates. <bold>NOT RECOMMENDED!</>"),
+            maps_processing_mode_arg_desc: cstr!("How to process maps.\ndefault - Ignore all previously encountered text duplicates\nseparate - For each new map, reset the set of previously encountered text duplicates <bold>RECOMMENDED!</>\npreserve - Allow all text duplicates. <bold>NOT RECOMMENDED!</>\nThis argument will automatically be set on write/read with --mode append/purge commands if you parsed game text with it."),
 
-            stat_arg_desc: "Outputs unused lines to stat.txt file, leaving translation unchanged.",
+            stat_arg_desc: "Outputs unused lines to stat.txt file, leaving translation unchanged. Incompatible with preserve maps processing mode.",
             leave_filled_flag_desc: "Doesn't purge the lines, that have translation.",
             purge_empty_flag_desc: "Purge only the lines, that don't have the translation.",
-            create_ignore_flag_desc: "Create an ignore file from purged lines, to prevent their further appearance when using --mode append.",
+            create_ignore_flag_desc: "Create an ignore file from purged lines, to prevent their further appearance when using --mode append. Incompatible with preserve maps processing mode.",
 
             generate_json_command_desc: cstr!(r#"<bold>Generates JSON representations of older engines' files in "json" directory.</>"#),
             write_json_command_desc: cstr!(r#"<bold>Writes JSON representations of older engines' files from "json" directory back to original files.</>"#),
+
+            version_flag_desc: "Show program's version.",
+            ignore_flag_desc: cstr!("Ignore entries from .rvpacker-ignore file. <bold>WORKS ONLY WITH --mode append!"),
 
             // Argument types
             mode_arg_type: "MODE",
@@ -159,13 +159,14 @@ impl Localization<'_> {
             could_not_determine_game_engine_msg: "Couldn't determine game engine. Check the existence of System file inside your data/original directory.",
             game_ini_file_missing_msg: "Game.ini file not found.",
             enabling_maps_processing_mode_metadata_msg: "Setting maps_processing_mode value to  according to the metadata from previous read.",
+            purge_args_incompatible_with_preserve_mode_msg: "--stat and --create-ignore arguments are incompatble with preserve maps processing mode.",
+            ignore_file_does_not_exist_msg: ".rvpacker-ignore file does not exist. Aborting execution.",
+            could_not_decrypt_ini_file_msg: "Couldn't decrypt Game.ini file. You can try to turn it UTF-8 yourself, after that everything will work.",
 
             // Misc
             possible_values: "Allowed values:",
             example: "Example:",
             default_value: "Default value:",
-            when_reading: "When reading:",
-            when_writing: "When writing:",
             aliases: "Aliases:"
         }
     }
@@ -173,7 +174,7 @@ impl Localization<'_> {
     const fn init_ru() -> Self {
         Localization {
             about_msg: cstr!(
-                "<bold>Инструмент, позволяющий парсить текст из файлов RPG Maker XP/VX/VXAce/MV/MZ игр в .txt файлы,                  а затем записывать их обратно в совместимые файлы.</>"
+                r#"<bold>Инструмент, позволяющий парсить текст из файлов RPG Maker XP/VX/VXAce/MV/MZ игр в .txt файлы, а затем записывать их обратно в совместимые файлы. Программа всегда будет использовать директории "original" или "data" для хранения исходных файлов, а также директорию "translation" для работы с файлами перевода. Программа также расшифрует любой зашифрованный архив .rgss, если он присутствует.</>"#
             ),
             help_template: cstr!(
                 "{about}\n\n<underline,bold>Использование:</> rvpacker-txt-rs КОМАНДА [ОПЦИИ]\n\n<underline,bold>Команды:</>\n{subcommands}\n\n<underline,bold>Опции:</>\n{options}"
@@ -192,24 +193,20 @@ impl Localization<'_> {
             json_help_template: cstr!("{about}\n\n<underline,bold>Использование:</> {usage}\n\n<underline,bold>Команды:</>\n{subcommands}\n\n<underline,bold>Опции:</>\n{options}"),
             json_command_desc: cstr!(r#"Предоставляет команды для генерации JSON файлов и их записи."#),
 
-            input_dir_arg_read_desc: r#"Входная директория, содержащая папку "origin al" или "data" ("Data") с оригинальными файлами игры."#,
-            input_dir_arg_write_desc: r#"Входная директория, содержащая папку "original" или "data" ("Data") с оригинальными файлами игры, а также папку "translation" с .txt файлами перевода."#,
-
-            output_dir_arg_read_desc: r#"Выходная директория, где будет создана папка "translation" с .txt файлами перевода."#,
-            output_dir_arg_write_desc: r#"Выходная директория, где будет создана папка "output" с подпапками "data" ("Data") и/или "js", содержащими игровые файлы с переведённым текстом из .txt файлов."#,
-
+            input_dir_arg_desc: r#"Входная директория, содержащая файлы игры."#,
+            output_dir_arg_desc: r#"Выходная директория, в которую будут помещены выходные файлы."#,
             disable_processing_arg_desc: "Не обрабатывает указанные файлы. plugins может применятся взаимозаменяемо со scripts.",
 
-            romanize_desc: r#"Если вы парсите текст из японской игры, содержащей символы вроде 「」, являющимися обычными японскими кавычками, программа автоматически заменяет эти символы на их европейские эквиваленты. (в данном случае, '')"#,
+            romanize_desc: "Если вы парсите текст из японской игры, содержащей символы вроде 「」, являющимися обычными японскими кавычками, программа автоматически заменяет эти символы на их европейские эквиваленты. (в данном случае, '').\nЭтот аргумент будет автоматически установлен при командах write/read вместе с --mode append/purge, если текст игры был прочитан с его использованием.",
 
-            disable_custom_processing_desc: "Отключает использование индивидуальных способов обработки текста, имплементированных для некоторых игр. Этот флаг будет автоматически применён при записи, если текст игры был прочитан с его использованием.",
+            disable_custom_processing_desc: "Отключает использование индивидуальных способов обработки текста, имплементированных для некоторых игр.\nЭтот аргумент будет автоматически установлен при командах write/read вместе с --mode append/purge, если текст игры был прочитан с его использованием.",
             language_arg_desc: "Устанавливает локализацию инструмента на выбранный язык.",
 
             log_arg_desc: "Включает логирование.",
             help_arg_desc: "Выводит справочную информацию по программе либо по введёной команде.",
 
             processing_mode_arg_desc: "Как обрабатывать файлы.\ndefault - Стандартный режим. Прекращает обработку, если .txt файлы перевода уже существуют.\nappend - Режим добавления. Например, если переводимая вами игра обновится, вы можете использовать этот аргумент чтобы добавить любой новый текст в существующие файлы, сохраняя порядок линий.\nforce - Принудительный режим. Принудительный режим перезаписывает существующие .txt файлы.",
-            maps_processing_mode_arg_desc: "Как обрабатывать карты.\ndefault - Игнорировать дубликаты всего ранее встреченного текста.\nseparate - Для каждой новой карты, сбрасывать список ранее встреченного текста. <bold>РЕКОМЕНДУЕТСЯ!</>\npreserve - Разрешить все дубликаты текста. <bold>НЕ РЕКОМЕНДУЕТСЯ!</>",
+            maps_processing_mode_arg_desc: "Как обрабатывать карты.\ndefault - Игнорировать дубликаты всего ранее встреченного текста.\nseparate - Для каждой новой карты, сбрасывать список ранее встреченного текста. <bold>РЕКОМЕНДУЕТСЯ!</>\npreserve - Разрешить все дубликаты текста. <bold>НЕ РЕКОМЕНДУЕТСЯ!</>\nЭтот аргумент будет автоматически установлен при командах write/read вместе с --mode append/purge, если текст игры был прочитан с его использованием.",
 
             stat_arg_desc: "Выводит неиспользуемые строки в файл stat.txt, не производя никаких изменений в файлах перевода.",
             leave_filled_flag_desc: "Удаляет только неиспользуемые строки без перевода.",
@@ -218,6 +215,9 @@ impl Localization<'_> {
 
             generate_json_command_desc: r#"Генерирует JSON-репрезентации файлов старых движков в директории "json"."#,
             write_json_command_desc: r#"Записывает JSON-репрезентации файлов старых движков из директории "json" обратно в исходные файлы."#,
+
+            version_flag_desc: "Отобразить версию программы.",
+            ignore_flag_desc: cstr!("Игнорировать строки из файла .rvpacker-ignore. <bold>РАБОТАЕТ ТОЛЬКО ПРИ --mode append!</>"),
 
             mode_arg_type: "РЕЖИМ",
             input_path_arg_type: "ВХОДНОЙ_ПУТЬ",
@@ -233,18 +233,19 @@ impl Localization<'_> {
             elapsed_time_msg: "Затраченное время:",
             force_mode_warning: "ПРЕДУПРЕЖДЕНИЕ! Принудительный режим полностью перепишет все ваши файлы перевода, включая _trans-файлы. Введите Y, чтобы продолжить.",
             custom_processing_enabled_msg: "Индивидуальная обработка текста будет использована для этой игры. Используйте --disable-custom-processing, чтобы отключить её.",
-            enabling_romanize_metadata_msg: "В соответствии с метаданными из прошлого чтения, романизация текста                                              будет использована.",
+            enabling_romanize_metadata_msg: "В соответствии с метаданными из прошлого чтения, романизация текста будет использована.",
             disabling_custom_processing_metadata_msg: "В соответсвии с метаданными из прошлого чтения, индивидуальная обработка текста будет выключена.",
             no_subcommand_specified_msg: "Команда не была указана. Вызовите rvpacker-txt-rs -h для помощи.",
             could_not_determine_game_engine_msg: "Не удалось определить движок игры. Убедитесь, что файл System существует.",
             game_ini_file_missing_msg: "Файл Game.ini не был обнаружен.",
             enabling_maps_processing_mode_metadata_msg: "Значение аргумента maps_processing_mode установлено на  в соответствии с метаданными из прошлого чтения.",
+            purge_args_incompatible_with_preserve_mode_msg: "Аргументы --stat и --create-ignore несовместимы с режимом обработки карт preserve.",
+            ignore_file_does_not_exist_msg: "Файл .rvpacker-ignore не существует. Прерываем выполнение.",
+            could_not_decrypt_ini_file_msg: "Не удалось расшифровать файл Game.ini. Вы можете вручную конвертировать его в UTF-8, после этого всё заработает.",
 
             possible_values: "Разрешённые значения:",
             example: "Пример:",
             default_value: "Значение по умолчанию:",
-            when_reading: "При чтении:",
-            when_writing: "При записи:",
             aliases: "Также:"
         }
     }
